5104181f726530afc811ee7684cc9efe893af536
karimhesham23 <kh488613@gmail.com>
2023-09-29 03:35:57 +0200

    add animation to splash view

--- to_do_list_app/lib/core/utils/routes.dart
+++ to_do_list_app/lib/core/utils/routes.dart
@@ -1,11 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:to_do_list_app/features/splash/presentation/views/splash_view.dart';
+import 'package:to_do_list_app/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 abstract class AppRoute {
+  static const kSplashView = "/";
   static final router = GoRouter(
     routes: [
       GoRoute(
-        path: '/',
+        name: SplashViewBody.id,
+        path: kSplashView,
         builder: (context, state) => const SplashView(),
       ),
     ],
--- to_do_list_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ to_do_list_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,32 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list_app/core/utils/assets.dart';
 import 'package:to_do_list_app/core/utils/styles.dart';
 
-class SplashViewBody extends StatelessWidget {
-  const SplashViewBody({super.key});
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+  static String id = "Splash View";
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration:
+          const Duration(milliseconds: 500), // Adjust the duration as needed.
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    // Start the animation after a short delay (e.g., 2 seconds).
+    Future.delayed(const Duration(seconds: 1), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         const SizedBox(
           width: double.infinity,
         ),
-        Image.asset(
-          AssetsApp.icon,
-          height: 181,
-          width: 181,
-        ),
-        Text(
-          "DO IT NOW",
-          style: Styles.logoTextSyle.copyWith(
-            fontSize: 34,
+        ScaleTransition(
+          scale: _scaleAnimation,
+          child: Image.asset(
+            AssetsApp.icon,
+            height: 181,
+            width: 156,
           ),
         ),
-        Text(
-          "YOUR TASKS, YOUR WAY",
-          style: Styles.logoTextSyle,
+        ScaleTransition(
+          scale: _scaleAnimation,
+          child: Text(
+            "DO IT NOW",
+            style: Styles.logoTextSyle.copyWith(
+              fontSize: 34,
+            ),
+          ),
+        ),
+        ScaleTransition(
+          scale: _scaleAnimation,
+          child: Text(
+            "YOUR TASKS, YOUR WAY",
+            style: Styles.logoTextSyle,
+          ),
         ),
       ],
     );
